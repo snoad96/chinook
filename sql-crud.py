e41a8c93504db15fdb95f2a20036f32810fba05f
DSnoad <danialsnoad@hotmail.com>
2023-01-06 11:55:58 +0000

    Add sql-crud file

---
+++ sql-crud.py
@@ -0,0 +1,60 @@
+from sqlalchemy import (
+    create_engine, Column, Integer, String
+)
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+# executing the instructions from the "chinook" database
+db = create_engine("postgresql:///chinook")
+base = declarative_base()
+
+
+# create a class-based model for the "programmer" table
+class Programmer(base):
+    __tablename__ = "Programmer"
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    gender = Column(String)
+    nationality = Column(String)
+    famous_for = Column(String)
+    
+
+# instead of connecting to the database directly, we will ask for a session
+# create a new instance of sessionmaker, then point to our engine (the db)
+Session = sessionmaker(db)
+# opens an actual session by calling the Session() subclass defined above
+session = Session()
+
+# creating the database using declarative_base subclass
+base.metadata.create_all(db)
+
+
+# creating records on our Programmer table
+ada_lovelace = Programmer(
+    first_name="Ada",
+    last_name="Lovelace",
+    gender="F",
+    nationality="British",
+    famous_for="First Programmer"
+)
+
+
+# add each instance of our programmers to our session
+session.add(ada_lovelace)
+
+# commit our session to the database
+session.commit()
+
+# query the databse to find all Programmers
+Programmers = session.query(Programmer)
+for programmer in programmers:
+    print(
+        programmer.id,
+        programmer.first_name + " " + programmer.last_name,
+        programmer.gender,
+        programmer.nationality,
+        programmer.famous_for,
+        sep=" | "
+    )
--- sql-orm.py
+++ sql-orm.py
@@ -72,7 +72,8 @@ base.metadata.create_all(db)
 # for album in albums:
 #     print(album.AlbumId, album.Title, album.ArtistId, sep=" | ")
 
-# Query 6 - select all tracks where the composer is "Queen" from the "Track" table
+# Query 6 - select all tracks where the composer is "Queen" from 
+# the "Track" table
 tracks = session.query(Track).filter_by(Composer="Queen")
 for track in tracks:
     print(
