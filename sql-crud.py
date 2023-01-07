d61217d80bdbb4f3096a5f06e689d2d0098e36b5
DSnoad <danialsnoad@hotmail.com>
2023-01-07 15:59:13 +0000

    Add famous programmers to the table

--- sql-crud.py
+++ sql-crud.py
@@ -40,9 +40,54 @@ ada_lovelace = Programmer(
     famous_for="First Programmer"
 )
 
+allan_turing = Programmer(
+    first_name="Allan",
+    last_name="Turing",
+    gender="M",
+    nationality="British",
+    famous_for="Modern Computing"
+)
+
+grace_hopper = Programmer(
+    first_name="Grace",
+    last_name="Hopper",
+    gender="F",
+    nationality="American",
+    famous_for="COBOL langauge"
+)
+
+margaret_hamilton = Programmer(
+    first_name="Margaret",
+    last_name="Hamilton",
+    gender="F",
+    nationality="American",
+    famous_for="Apollo 11"
+)
+
+bill_gates = Programmer(
+    first_name="Bill",
+    last_name="Gates",
+    gender="M",
+    nationality="American",
+    famous_for="Microsoft"
+)
+
+tim_berners_lee = Programmer(
+    first_name="Ted",
+    last_name="Berners_Lee",
+    gender="M",
+    nationality="British",
+    famous_for="World Wide Web"
+)
+
 
 # add each instance of our programmers to our session
 session.add(ada_lovelace)
+session.add(allan_turing)
+session.add(grace_hopper)
+session.add(margaret_hamilton)
+session.add(bill_gates)
+session.add(tim_berners_lee)
 
 # commit our session to the database
 session.commit()
